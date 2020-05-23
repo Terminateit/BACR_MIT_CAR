Fixed the time calculation. Changed the names for the collected snapshots.

racecar/racecar_v2/envs/racecar_v2_env.py
@@ -6,7 +6,6 @@ from gym import error, spaces, utils
 from gym.utils import seeding
 
 import os
-import time
 import numpy as np
 import csv
 from PIL import Image
@@ -46,8 +45,6 @@ class CarRaceEnv(gym.Env):
         # Observation space - camera snapshot
         self.observation_space = spaces.Box(low=0, high=255, shape=(64, 64, 3), dtype=np.uint8)
 
-        self.snapshotNumber = 0
-
         # Joint numbers
         self.camera_joint = 5
         self.rearWheels = [8, 15]
@@ -66,6 +63,7 @@ class CarRaceEnv(gym.Env):
 
 
     def createWorld(self, model_name, track_name):
+        self.resetCounter = 0
 
         if model_name is None:
             model_name = 'racecar_differential.urdf'
@@ -94,22 +92,23 @@ class CarRaceEnv(gym.Env):
             p.getJointInfo(self.car, wheel)
 
         c = p.createConstraint(self.car, 9, self.car, 11, jointType=p.JOINT_GEAR, jointAxis=[0,1,0], parentFramePosition=[0,0,0], childFramePosition=[0,0,0])
-        p.changeConstraint(c,gearRatio=1, maxForce=10000)
+        p.changeConstraint(c, gearRatio=1, maxForce=10000)
         c = p.createConstraint(self.car, 10, self.car, 13, jointType=p.JOINT_GEAR, jointAxis=[0,1,0], parentFramePosition=[0,0,0], childFramePosition=[0,0,0])
-        p.changeConstraint(c,gearRatio=-1, maxForce=10000)
+        p.changeConstraint(c, gearRatio=-1, maxForce=10000)
         c = p.createConstraint(self.car, 9, self.car, 13, jointType=p.JOINT_GEAR, jointAxis=[0,1,0], parentFramePosition=[0,0,0], childFramePosition=[0,0,0])
-        p.changeConstraint(c,gearRatio=-1, maxForce=10000)
+        p.changeConstraint(c, gearRatio=-1, maxForce=10000)
         c = p.createConstraint(self.car, 16, self.car, 18, jointType=p.JOINT_GEAR, jointAxis=[0,1,0], parentFramePosition=[0,0,0], childFramePosition=[0,0,0])
-        p.changeConstraint(c,gearRatio=1, maxForce=10000)
+        p.changeConstraint(c, gearRatio=1, maxForce=10000)
         c = p.createConstraint(self.car, 16, self.car, 19, jointType=p.JOINT_GEAR, jointAxis=[0,1,0], parentFramePosition=[0,0,0], childFramePosition=[0,0,0])
-        p.changeConstraint(c,gearRatio=-1, maxForce=10000)
+        p.changeConstraint(c, gearRatio=-1, maxForce=10000)
         c = p.createConstraint(self.car, 17, self.car, 19, jointType=p.JOINT_GEAR, jointAxis=[0,1,0], parentFramePosition=[0,0,0], childFramePosition=[0,0,0])
-        p.changeConstraint(c,gearRatio=-1, maxForce=10000)
+        p.changeConstraint(c, gearRatio=-1, maxForce=10000)
 
         c = p.createConstraint(self.car, 1, self.car, 18, jointType=p.JOINT_GEAR, jointAxis=[0,1,0], parentFramePosition=[0,0,0], childFramePosition=[0,0,0])
-        p.changeConstraint(c,gearRatio=-1, gearAuxLink = 15, maxForce=10000)
+        p.changeConstraint(c, gearRatio=-1, gearAuxLink=15, maxForce=10000)
         c = p.createConstraint(self.car, 3, self.car, 19, jointType=p.JOINT_GEAR, jointAxis=[0,1,0], parentFramePosition=[0,0,0], childFramePosition=[0,0,0])
-        p.changeConstraint(c,gearRatio=-1, gearAuxLink = 15, maxForce=10000)
+        p.changeConstraint(c, gearRatio=-1, gearAuxLink=15, maxForce=10000)
+
 
 
     def reset(self, model_name=None, track_name=None, cameraStatus=True, storeData=False):
@@ -121,12 +120,15 @@ class CarRaceEnv(gym.Env):
         self.cameraStatus = cameraStatus
         self.cameraDirectory = 'snapshots'
 
-        self.stuckCounter = 0 # reset stuck counter
+        # reset counters
+        self.stuckCounter = 0
+        self.stepCounter = 0
 
         if self.world_does_exist is False:
             self.createWorld(model_name, track_name)
             self.world_does_exist = True
         else:
+            self.resetCounter += 1
             carPosition = [0, 0, 0.15]
             carOrientation = p.getQuaternionFromEuler([0, 0, np.pi/3])
             p.resetBasePositionAndOrientation(self.car, carPosition, carOrientation)
@@ -147,15 +149,10 @@ class CarRaceEnv(gym.Env):
 
             self.snapshot, self.snapshotPath, self.nextSnapshotPath = self.takeSnapshot()
 
-        self.lastControlTime = 0
-        self.lastCameraTime = 0
-        self.currentTime = 0
-
         return self.snapshot
 
 
     def act(self):
-        self.lastControlTime = self.currentTime
         for wheel in self.rearWheels:
             p.setJointMotorControl2(self.car, wheel, p.VELOCITY_CONTROL, targetVelocity=self.velocity, force=self.force)
             
@@ -164,23 +161,19 @@ class CarRaceEnv(gym.Env):
 
 
     def step(self, action):
-        # Apply control action (120 Hz)
-        if (self.currentTime - self.lastControlTime > self.dt):
+        # Apply control action (120 Hz, each 1st step)
+        if (self.stepCounter % 1 == 0):
             self.velocity = action[0]
             self.steeringAngle = action[1]
             self.force = action[2]
             self.act()
 
-        # Update camera data (130 Hz)
+        # Update camera data (120 Hz, each 1st step)
         if self.cameraStatus is True:
-            if (self.currentTime - self.lastCameraTime > 1/130.):
-                self.lastCameraTime = self.currentTime
-
+            if (self.stepCounter % 1 == 0):
                 self.snapshot, self.snapshotPath, self.nextSnapshotPath = self.takeSnapshot()
 
         p.stepSimulation()
-
-        self.currentTime += self.dt
 
         carVelocity = p.getBaseVelocity(self.car)[0] # get linear velocity only
         carSpeed = np.linalg.norm(carVelocity)
@@ -198,6 +191,9 @@ class CarRaceEnv(gym.Env):
         if self.storeData is True:
             datasetRow = [self.snapshotPath, action, reward, int(done), self.nextSnapshotPath]
             self.dataset.writerow(datasetRow)
+
+        # Update step counter
+        self.stepCounter += 1
 
         return self.snapshot, reward, done, {}
     
@@ -237,11 +233,10 @@ class CarRaceEnv(gym.Env):
 
         rgbImg = Image.fromarray(rgbaImg).convert('RGB')
         
-        savePath = os.path.join(self.cameraDirectory, 'snapshot' + str(self.snapshotNumber) + '.jpg')
-        nextPath = os.path.join(self.cameraDirectory, 'snapshot' + str(self.snapshotNumber+1) + '.jpg')
+        savePath = os.path.join(self.cameraDirectory, 'snapshot_' + str(self.resetCounter) + '_' + str(self.stepCounter) + '.jpg')
+        nextPath = os.path.join(self.cameraDirectory, 'snapshot_' + str(self.resetCounter) + '_' + str(self.stepCounter + 1) + '.jpg')
         if self.storeData is True:
             rgbImg.save(savePath, "JPEG")
-            self.snapshotNumber += 1
 
         rgbImg = np.array(rgbImg)
         
@@ -249,13 +244,13 @@ class CarRaceEnv(gym.Env):
 
 
     def render(self):
-        #if self.cameraStatus is True:
+        # if self.cameraStatus is True:
         #    from gym.envs.classic_control import rendering
         #    if self.viewer is None:
         #        self.viewer = rendering.SimpleImageViewer()
         #    self.viewer.imshow(self.snapshot)
         #    return self.viewer.isopen
-        #else:
+        # else:
             return self.snapshot
 
     def close(self):